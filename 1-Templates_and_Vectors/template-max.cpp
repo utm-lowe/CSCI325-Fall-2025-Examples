f04566e5c0523ccd771ec754b17ff71e09a8a504
Robert Lowe <pngwen@gmail.com>
2025-08-27 01:19:41 +0000

    examples for templates

---
+++ 1-Templates_and_Vectors/template-max.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file template-max.cpp
+ * @author Bob Lowe <rlowe8@utm.edu>
+ * @brief A demonstration of templates
+ * @version 0.1
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <iostream>
+
+template <typename T>
+T max(T a, T b) 
+{
+    if(a>b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+int main() 
+{
+    std::cout << max(3, 7) << std::endl;        // int version
+    std::cout << max(3.14, 2.71) << std::endl;  // double version
+}
