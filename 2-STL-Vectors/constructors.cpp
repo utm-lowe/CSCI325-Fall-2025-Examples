0a5dab48e6f8c0cd332df3d44bf2f55807c7b228
Robert Lowe <pngwen@gmail.com>
2025-09-03 15:51:00 +0000

    vector examples

--- 2-STL-Vectors/constructors.cpp
+++ 2-STL-Vectors/constructors.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <string>
+
 
 int main() {
-  vector<int> a;                 // default
-  vector<int> b(5);              // 5 zeros
-  vector<int> c(5, 7);           // 5 sevens
-  vector<int> d{1,2,3,4};        // list-init
+  std::vector<int> a;                 // default
+  std::vector<int> b(5);              // 5 zeros
+  std::vector<int> c(5, 7);           // 5 sevens
+  std::vector<int> d{1,2,3,4};        // list-init
+  std::vector<std::string> s(5, "chicken");
 
-  cout << "a.size=" << a.size() << "\n";
-  cout << "b: "; for (int x: b) cout << x << ' '; cout << "\n";
-  cout << "c: "; for (int x: c) cout << x << ' '; cout << "\n";
-  cout << "d: "; for (int x: d) cout << x << ' '; cout << "\n";
+  std::cout << "a.size=" << a.size() << "\n";
+
+  std::cout << "b: "; 
+  
+  // This is a C++ foreach loop
+  // You can do this with any iterator based container
+  for (int x: b) {
+    std::cout << x << ' '; 
+  }
+
+  /* The above is shorthand for
+     for(std::vector<int>::iterator it = b.begin(); it != b.end(); it++) {
+       int x = *it;
+       std::cout << x << "\n";
+     }
+  */
+
+  std::cout << "\n";
+  
+  std::cout << "c: "; for (int x: c) std::cout << x << ' '; std::cout << "\n";
+  std::cout << "d: "; for (int x: d) std::cout << x << ' '; std::cout << "\n";
+  std::cout << "s: "; for (std::string &x: s) std::cout << x << ' '; std::cout << "\n";
 }
--- 2-STL-Vectors/size_capacity.cpp
+++ 2-STL-Vectors/size_capacity.cpp
@@ -8,10 +8,10 @@ int main() {
        << " cap=" << v.capacity() << "\n";
   for (int i = 1; i <= 16; ++i) {
     v.push_back(i);
-    if (i == 1 || (i & (i - 1)) == 0) { // print at 1,2,4,8,16
+    //if (i == 1 || (i & (i - 1)) == 0) { // print at 1,2,4,8,16
       cout << "i=" << i
            << " size=" << v.size()
            << " cap=" << v.capacity() << "\n";
-    }
+    //}
   }
 }
